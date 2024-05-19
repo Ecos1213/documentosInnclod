09cf7dccccc998475eda23d6a7c2a348c4d2a53f
Ecos1213 <diasflac1213@gmail.com>
2024-05-19 13:08:11 -0500

    Cambia dashboard, rutas, grupo del middelware: cambia la pagina de dashboard para que contenga la tabla de grilla de productos, cambia las rutas para que el login sea la pagina inicial si no esta logeado y grupo de middleware el regisgtro pasa de guest a auth para que solo personas logueadas puedan crear registros y cambia el seeder para que cree un usuario estandar para realizar el testing

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
     return (
@@ -11,8 +11,159 @@ export default function Dashboard({ auth }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    <div className="overflow-hidden shadow-sm sm:rounded-lg">
+                        
+                        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+                                <div>
+                                    <Link>
+                                        <button type="button" className="text-blue-700 hover:text-white border border-blue-700 
+                                                                        hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
+                                                                        font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 
+                                                                        dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                                            Crear Documento
+                                        </button>
+                                    </Link>
+                                </div>
+                                <div className="bg-white dark:bg-gray-900">
+                                    <label for="table-search" className="sr-only">Search</label>
+                                    <div className="relative mt-1">
+                                        <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                            </svg>
+                                        </div>
+                                        <input type="text" id="table-search" className="block mb-1 pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items" />
+                                    </div>
+                                </div>
+                            </div>
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-3">
+                                            Product name
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Color
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Category
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Price
+                                        </th>
+                                        <th scope="col" className="px-6 py-3">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Apple MacBook Pro 17"
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            Laptop
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $2999
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Microsoft Surface Pro
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            White
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            Laptop PC
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $1999
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Magic Mouse 2
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            Black
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            Accessories
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $99
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Apple Watch
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            Accessories
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $179
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            iPad
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            Gold
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            Tablet
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $699
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Apple iMac 27"
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            PC Desktop
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            $3999
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
             </div>
