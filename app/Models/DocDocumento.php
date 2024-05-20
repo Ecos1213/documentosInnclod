6174b6042a7b4568c19a84ca7080e66c71c1f9db
Ecos1213 <diasflac1213@gmail.com>
2024-05-19 20:27:33 -0500

    Cambia modelos: cambia los modelos docdocumento, proproceso y tiptipodoc agrega los campos que se pueden rellenar y los que no en docdocumento y sus respectivas relaciones

--- app/Models/DocDocumento.php
+++ app/Models/DocDocumento.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DocDocumento extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'doc_id_tipo',
+        'doc_id_proceso',
+        'doc_nombre',
+        'doc_codigo',
+        'doc_contenido'
+    ];
+
+    protected $guarded = [
+        'id',
+        'doc_id_tipo',
+        'doc_id_proceso',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function proproceso(): BelongsTo {
+        return $this->belongsTo(ProProceso::class);
+    }
+
+    public function tiptipodoc(): BelongsTo {
+        return $this->belongsTo(TipTipoDoc::class);
+    }
 }
